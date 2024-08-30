d5ddd5890396ec90627762ec06f55bb8cffb27bc
Olafcio <olafcio@int.pl>
2024-08-30 22:45:57 +0200

    FUCK the :create command was fucked. i never tested 1.1 tbh and this sucks i should've did.

--- src/main/java/pl/olafcio/lprm/mixins/ChatScreenMixin.java
+++ src/main/java/pl/olafcio/lprm/mixins/ChatScreenMixin.java
@@ -2,13 +2,11 @@ package pl.olafcio.lprm.mixins;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Objects;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import pl.olafcio.lprm.GradientUtil;
 import pl.olafcio.lprm.Main;
 import pl.olafcio.lprm.MyUtils;
@@ -24,84 +22,96 @@ public class ChatScreenMixin {
             ArrayList<String> args = new ArrayList<>(Arrays.stream(chatText.substring(1).split(" ")).toList());
             String cmd = args.removeFirst();
 
-            if (Objects.equals(cmd, "idcreate")) {
-                if (args.size() < 4) {
-                    Main.mc.player.sendMessage(Text.of("§6[LPRM]§7 Usage: §lidcreate [rank ID] {format codes + rank name} [gradient color1 hex] [gradient color2 hex]"));
-                    return;
-                }
-
-                String gradc2 = args.removeLast(),
-                       gradc1 = args.removeLast(),
-                       id = args.removeFirst();
-                var text = String.join(" ", args);
-                text = MyUtils.smallCaps(text);
-                var formatCodes = "";
-                var inColor = false;
-                for (var ch : text.split("")) {
-                    if (inColor)
-                        formatCodes += "&" + ch;
-                    else if (ch == "&")
-                        inColor = true;
-                    else break;
-                }
-                String gradient = GradientUtil.generateMessage(gradc1, gradc2, text, formatCodes);
-                gradient = gradient.replace("\"", "\\\"");
-                String finalGradient = gradient;
-                (new Thread(() -> {
-                    try {
-                        Main.mc.player.networkHandler.sendCommand("lp creategroup " + id);
-                        Thread.sleep(600L);
-                        Main.mc.player.networkHandler.sendCommand("lp group " + id + " setdisplayname \"" + finalGradient + "\"");
-                        Thread.sleep(600L);
-                        Main.mc.player.networkHandler.sendCommand("lp group " + id + " meta setprefix 90 \"" + finalGradient + "&7 »&r \"");
-                        Thread.sleep(500L);
-                        Main.mc.player.sendMessage(Text.of("§6[LPRM]§7 Rank created. It has been given the ID: §l" + id));
-                    } catch (InterruptedException var3) {
-                        InterruptedException e = var3;
-                        throw new RuntimeException(e);
+            switch (cmd) {
+                case "idcreate" -> {
+                    if (args.size() < 4) {
+                        Main.mc.player.sendMessage(Text.of("§6[LPRM]§7 Usage: §lidcreate [rank ID] {format codes + rank name} [gradient color1 hex] [gradient color2 hex]"));
+                        return;
                     }
-                })).start();
-            } else if (Objects.equals(cmd, "create")) {
-                if (args.size() < 4) {
-                    Main.mc.player.sendMessage(Text.of("§6[LPRM]§7 Usage: §lcreate {format codes + rank name} [gradient color1 hex] [gradient color2 hex] [format codes]"));
-                    return;
-                }
 
-                String formatCodes = args.removeLast(),
-                       gradc2 = args.removeLast(),
-                       gradc1 = args.removeLast();
-                var text = String.join(" ", args);
-                var id = text.replace(" ", "-").replace("_", "-").replace(" ", "-");
-                text = MyUtils.smallCaps(text);
-                String gradient = GradientUtil.generateMessage(gradc1, gradc2, text, formatCodes);
-                gradient = gradient.replace("\"", "\\\"");
-                String finalGradient = gradient;
-                (new Thread(() -> {
-                    try {
-                        Main.mc.player.networkHandler.sendCommand("lp creategroup " + id);
-                        Thread.sleep(600L);
-                        Main.mc.player.networkHandler.sendCommand("lp group " + id + " setdisplayname \"" + finalGradient + "\"");
-                        Thread.sleep(600L);
-                        Main.mc.player.networkHandler.sendCommand("lp group " + id + " meta setprefix 90 \"" + finalGradient + "&7 »&r \"");
-                        Thread.sleep(500L);
-                        Main.mc.player.sendMessage(Text.of("§6[LPRM]§7 Rank created. It has been given the ID: §l" + id));
-                    } catch (InterruptedException var3) {
-                        InterruptedException e = var3;
-                        throw new RuntimeException(e);
+                    String gradc2 = args.removeLast(),
+                            gradc1 = args.removeLast(),
+                            id = args.removeFirst();
+                    var text = String.join(" ", args);
+                    text = MyUtils.smallCaps(text);
+                    var formatCodes = "";
+                    var inColor = false;
+                    for (var ch : text.split("")) {
+                        if (inColor)
+                            formatCodes += "&" + ch;
+                        else if (ch == "&")
+                            inColor = true;
+                        else break;
                     }
-                })).start();
-            } else if (Objects.equals(cmd, "grant")) {
-                if (args.size() != 2) {
-                    Main.mc.player.sendMessage(Text.of("§6[LPRM]§7 Usage: §lgrant [player name] [rank id]"));
-                    return;
+                    String gradient = GradientUtil.generateMessage(gradc1, gradc2, text, formatCodes);
+                    gradient = gradient.replace("\"", "\\\"");
+                    String finalGradient = gradient;
+                    (new Thread(() -> {
+                        try {
+                            Main.mc.player.networkHandler.sendCommand("lp creategroup " + id);
+                            Thread.sleep(600L);
+                            Main.mc.player.networkHandler.sendCommand("lp group " + id + " setdisplayname \"" + finalGradient + "\"");
+                            Thread.sleep(600L);
+                            Main.mc.player.networkHandler.sendCommand("lp group " + id + " meta setprefix 90 \"" + finalGradient + "&7 »&r \"");
+                            Thread.sleep(500L);
+                            Main.mc.player.sendMessage(Text.of("§6[LPRM]§7 Rank created. It has been given the ID: §l" + id));
+                        } catch (InterruptedException var3) {
+                            InterruptedException e = var3;
+                            throw new RuntimeException(e);
+                        }
+                    })).start();
                 }
+                case "create" -> {
+                    if (args.size() < 4) {
+                        Main.mc.player.sendMessage(Text.of("§6[LPRM]§7 Usage: §lcreate {format codes + rank name} [gradient color1 hex] [gradient color2 hex] [format codes]"));
+                        return;
+                    }
 
-                var var10000 = Main.mc.player.networkHandler;
-                String var10001 = args.get(0);
-                var10000.sendCommand("lp user " + var10001 + " parent set " + args.get(1));
-                Main.mc.player.sendMessage(Text.of("Rank granted."));
-            } else {
-                Main.mc.player.sendMessage(Text.of("§6[LPRM]§7 Unknown command. To see all commands, run §l:help§7."));
+                    String gradc2 = args.removeLast(),
+                           gradc1 = args.removeLast();
+                    var text = String.join(" ", args);
+                    var id = text.replace(" ", "-").replace("_", "-").replace(" ", "-");
+                    text = MyUtils.smallCaps(text);
+                    var formatCodes = "";
+                    var inColor = false;
+                    for (var ch : text.split("")) {
+                        if (inColor)
+                            formatCodes += "&" + ch;
+                        else if (ch == "&")
+                            inColor = true;
+                        else break;
+                    }
+                    String gradient = GradientUtil.generateMessage(gradc1, gradc2, text, formatCodes);
+                    gradient = gradient.replace("\"", "\\\"");
+                    String finalGradient = gradient;
+                    (new Thread(() -> {
+                        try {
+                            Main.mc.player.networkHandler.sendCommand("lp creategroup " + id);
+                            Thread.sleep(600L);
+                            Main.mc.player.networkHandler.sendCommand("lp group " + id + " setdisplayname \"" + finalGradient + "\"");
+                            Thread.sleep(600L);
+                            Main.mc.player.networkHandler.sendCommand("lp group " + id + " meta setprefix 90 \"" + finalGradient + "&7 »&r \"");
+                            Thread.sleep(500L);
+                            Main.mc.player.sendMessage(Text.of("§6[LPRM]§7 Rank created. It has been given the ID: §l" + id));
+                        } catch (InterruptedException var3) {
+                            InterruptedException e = var3;
+                            throw new RuntimeException(e);
+                        }
+                    })).start();
+                }
+                case "grant" -> {
+                    if (args.size() != 2) {
+                        Main.mc.player.sendMessage(Text.of("§6[LPRM]§7 Usage: §lgrant [player name] [rank id]"));
+                        return;
+                    }
+
+                    var var10000 = Main.mc.player.networkHandler;
+                    String var10001 = args.get(0);
+                    var10000.sendCommand("lp user " + var10001 + " parent set " + args.get(1));
+                    Main.mc.player.sendMessage(Text.of("Rank granted."));
+                }
+                case null, default ->
+                        Main.mc.player.sendMessage(Text.of("§6[LPRM]§7 Unknown command. To see all commands, run §l:help§7."));
             }
         }
     }
